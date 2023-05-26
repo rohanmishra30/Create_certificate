90d8b5da30e49579acc009f2aee273bbbaa40adf
rohanmishra30 <91248195+rohanmishra30@users.noreply.github.com>
2023-05-27 00:19:41 +0530

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,7 @@
+var name=document.getElementById("name").value;
+var course=document.getElementById("course").value;
+function result(){
+alert("Scroll Down Your Certificate is ready");
+document.getElementById("user_name").innerHTML=name;
+document.getElementById("course_name").innerHTML=course;
+}
